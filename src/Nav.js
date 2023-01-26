3a3c3c7ea941b269ce87f527c79f171b4ff1de5f
Ludovico Pio Gnerre <ellpgi@gmail.com>
2023-01-26 16:47:59 +0100

    respondve nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import './Nav.css'
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 
-export default function Nav  () {
+
+export default function Nav  () { 
   return (
-<nav>   <div className="logo-container">
+    <>
+    <div className='menu-responsive'>
+
+    <ul>
+        <li><a href='#about'>About</a></li>
+        <li>< a href='#projects'>My Projects</a></li>
+
+        <li>< a href='#contact'>Contact me</a></li>
+
+        </ul>
+        <button>X</button>
+    </div>
+   
+<nav>   
+    
+  <div className="logo-container">
         <h2 id='logo'><a href='#about'>Ludovico Pio Gnerre</a></h2>
         </div>
+        <div className='burger'><RxHamburgerMenu></RxHamburgerMenu></div>
         <ul>
         <li><a href='#about'>About</a></li>
         <li>< a href='#projects'>My Projects</a></li>
@@ -15,5 +33,6 @@ export default function Nav  () {
 
         </ul>
        </nav>  
+       </>
        )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import './App.css'
-
+import { FaGithub } from 'react-icons/fa'
 
 
 
@@ -18,7 +18,9 @@ export default function Projects  () {
    </div>
    <div className='projects'>
     <div className="cards">
-      <div className='card'><a href="https://todolistellepigi.netlify.app" target="_blank">
+      <div className='card'><a href="https://github.com/ellepigi/esercizi/tree/main/todolist" target="_blank">
+        <div className='github-icon'><FaGithub></FaGithub></div></a>
+        <a href="https://todolistellepigi.netlify.app" target="_blank">
         <img src="/projects/todo.png" alt="#"/></a></div>
       <p>Just a very simple To do list...
 </p>
@@ -30,9 +32,12 @@ export default function Projects  () {
 </div>
     </div>
     <div className="cards">
-      <div className='card'><a href="https://reactshop-ellepigi.netlify.app/" target="_blank">
+      <div className='card'><a href="https://github.com/ellepigi/react-shop-project" target="_blank">
+        <div className='github-icon'><FaGithub></FaGithub></div></a>
+        <a href="https://reactshop-ellepigi.netlify.app/" target="_blank">
         <img src="/projects/ecommerce.png" alt="#"/></a></div>
-      <p>An ecommerce made with React... still needs some changes though!
+      <p>An ecommerce made with React, i used "Fake Store" api for the products and npm react-cart
+        for the cart... still needs some changes though!
 </p>
 <div className="buttons">
 <button disabled className='react'>React</button>
