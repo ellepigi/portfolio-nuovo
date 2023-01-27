5ad80c3d4c0c4ae19c16bc5efa90a8ddde6492a7
Ludovico Pio Gnerre <ellpgi@gmail.com>
2023-01-27 18:43:14 +0100

    bottoni risolto

--- src/Projects.js
+++ src/Projects.js
@@ -78,7 +78,7 @@ export default function Projects  () {
         for the cart... still needs some changes though!
 </p>
 <div className="buttons">
-<button disabled className='react'>React</button>
+<button disabled>React</button>
 </div>
     </div>
     
