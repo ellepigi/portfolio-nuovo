e2304c9f6c9e7e246c94f299b1c1c55ed33fae0b
Ludovico Pio Gnerre <ellpgi@gmail.com>
2023-01-29 20:34:25 +0100

    sito ultimato

--- src/Nav.js
+++ src/Nav.js
@@ -6,46 +6,46 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 export default function Nav  () { 
 
 
-  // const [notInView, setNotInView] = useState(true);
+  const [notInView, setNotInView] = useState(true);
 
 
-  // const navRef= useRef()
-  // const mainNavRef= useRef()
+  const navRef= useRef()
+  const mainNavRef= useRef()
 
-  // const showNav= () => {
-  //   navRef.current.classList.add("active")
-  // }
+  const showNav= () => {
+    navRef.current.classList.add("active")
+  }
 
-  // const removeNav= ()=>{
-  //   navRef.current.classList.remove("active")
+  const removeNav= ()=>{
+    navRef.current.classList.remove("active")
 
-  // }
+  }
 
 
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(
-  //     ([entry]) => {
-  //       if (!entry.isIntersecting) {
-  //         setNotInView(false);
-  //         mainNavRef.current.classList.add("nav-sticky");
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) {
+          setNotInView(false);
+          mainNavRef.current.classList.add("nav-sticky");
 
-  //       } else {
-  //         setNotInView(true);
-  //       }
-  //     },
-  //     { threshold: [0] }
-  //   );
-  //   if (mainNavRef.current) {
-  //     observer.observe(mainNavRef.current);
-  //   }
+        } else {
+          setNotInView(true);
+        }
+      },
+      { threshold: [0] }
+    );
+    if (mainNavRef.current) {
+      observer.observe(mainNavRef.current);
+    }
    
-  // }, []);
+  }, []);
 
 
 
   return (
     <>
-    <div   className='menu-responsive'>
+    <div ref={navRef} className='menu-responsive'>
 
     <ul>
         <li><a href='#about'>About</a></li>
@@ -54,15 +54,15 @@ export default function Nav  () {
         <li>< a href='#contact'>Contact me</a></li>
 
         </ul>
-        <button >X</button>
+        <button onClick={removeNav}>X</button>
     </div>
    
-<nav >   
+<nav ref={mainNavRef}>   
     
   <div className="logo-container">
         <h2 id='logo'><a href='#about'>Ludovico Pio Gnerre</a></h2>
         </div>
-        <div className='burger' ><RxHamburgerMenu></RxHamburgerMenu></div>
+        <div className='burger' onClick={showNav}><RxHamburgerMenu></RxHamburgerMenu></div>
         <ul>
         <li><a href='#section-skills'> My Skills</a></li>
         <li>< a href='#projects'>My Projects</a></li>
